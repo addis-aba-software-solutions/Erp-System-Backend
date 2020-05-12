91bfe5f4b7d26aa57774f6599119f9b0c3c605a2
endalk <endalkhbel@gmail.com>
2020-05-12 08:16:53 +0300

    result

--- app/settings.py
+++ app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
-# import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -136,4 +136,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATIC_URL = "/static/"
 WSGI_APPLICATION = "app.wsgi.application"
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
